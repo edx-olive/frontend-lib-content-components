4fcf1ce8e36a01027959e9022d5e31f460dd9780
shaharpcampusil <shaharp.campus.gov.il>
2026-04-11 09:28:41 +0300

    remove deploy test

--- dist/editors/containers/VideoEditor/components/VideoSettingsModal/components/TranscriptWidget/Transcript.js
+++ dist/editors/containers/VideoEditor/components/VideoSettingsModal/components/TranscriptWidget/Transcript.js
@@ -51,8 +51,7 @@ const Transcript = ({
     launchDeleteConfirmation,
     cancelDelete
   } = _module.hooks.setUpDeleteConfirmation();
-  const isSharedWarning = true; // sharedVideoWarning && sharedVideoWarning.language === language;
-
+  const isSharedWarning = sharedVideoWarning && sharedVideoWarning.language === language;
   if (isSharedWarning) {
     return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_paragon.Card, {
       className: "mb-2",
